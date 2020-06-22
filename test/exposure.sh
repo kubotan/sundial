79c58b18f7322ce6ed9db3cb15601b300912b048
kubotan <admin@gorogoro.space>
2020-06-23 02:42:13 +0900

    Update exposure.sh

--- test/exposure.sh
+++ test/exposure.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 
-# */1 * * * * bash /root/exposure.sh
+# */30 * * * * bash /root/exposure.sh
 # v4l2-ctl -d /dev/video0 --list-ctrls-menu
 
-startOfDay=`date "+%Y/%m/%d"`
-timestampStartOfDay=`date +%s -d $startOfDay`
-isDay=`/usr/local/bin/sundial 35.690 139.692 $timestampStartOfDay`
-elapsedTime=$(expr $(date +%s) - $(date +%s -r ./exposure.pre))
+OFFSET=$((30 * 60))
+SUNDIAL=`./sundial 35.690 139.692`
+SUNRISE=`echo $SUNDIAL | cut -d ',' -f 1`
+SUNSET=`echo $SUNDIAL | cut -d ',' -f 2`
+NOW=`date +%s`
 
-if [ $isDay -eq `cat ./exposure.pre` -o $elapsedTime -lt 1800 ]
+#date "+%Y/%m/%d %H:%M:%S" --date @$NOW
+#date "+%Y/%m/%d %H:%M:%S" --date @$(($SUNRISE + $OFFSET))
+#date "+%Y/%m/%d %H:%M:%S" --date @$(($SUNSET - $OFFSET))
+
+if [ $(($SUNRISE + $OFFSET)) -le $NOW -a $NOW -le $(($SUNSET - $OFFSET)) ]
 then
-    exit
+  isDay=1
+else
+  isDay=0
+fi
+
+if [ $isDay -eq `cat ./exposure.pre` ]
+then
+  exit
 fi
 echo $isDay > ./exposure.pre
 
@@ -31,7 +43,7 @@ else
     # day
 
     # mjpg-streamer1
-    v4l2-ctl -d /dev/video0 -c exposure_time_absolute=1000   # min=1 max=10000 step=1 default=1000
+    v4l2-ctl -d /dev/video0 -c exposure_time_absolute=50   # min=1 max=10000 step=1 default=1000
     v4l2-ctl -d /dev/video0 -c auto_exposure=1   # 0: Auto Mode 1: Manual Mode default=0
 
     # mjpg-streamer2
