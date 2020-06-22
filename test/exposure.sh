da5b2d97615160a86f1ab031e3864f6a6d7ad873
kubotan <admin@gorogoro.space>
2020-06-23 03:04:25 +0900

    Update exposure.sh

--- test/exposure.sh
+++ test/exposure.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# */30 * * * * bash /root/exposure.sh
+# */1 * * * * bash /root/exposure.sh
 # v4l2-ctl -d /dev/video0 --list-ctrls-menu
 
 OFFSET=$((30 * 60))
@@ -20,7 +20,12 @@ else
   isDay=0
 fi
 
-if [ $isDay -eq `cat ./exposure.pre` ]
+if [ ! -f ./exposure.pre ]
+then
+  echo 9 > ./exposure.pre
+fi
+
+if [ -f ./exposure.pre -a $isDay -eq `cat ./exposure.pre` ]
 then
   exit
 fi
@@ -31,24 +36,24 @@ then
     # night
 
     # mjpg-streamer1
-    v4l2-ctl -d /dev/video0 -c auto_exposure=1   # 0: Auto Mode 1: Manual Mode default=0
-    v4l2-ctl -d /dev/video0 -c exposure_time_absolute=10000   # min=1 max=10000 step=1 default=1000
+    #v4l2-ctl -d /dev/video0 -c auto_exposure=1   # 0: Auto Mode 1: Manual Mode default=0
+    #v4l2-ctl -d /dev/video0 -c exposure_time_absolute=10000   # min=1 max=10000 step=1 default=1000
 
     # mjpg-streamer2
-    #v4l2-ctl -d /dev/video0 -c exposure_auto=1   # min=78 max=1250 step=1 default=312
-    #v4l2-ctl -d /dev/video0 -c exposure_absolute=1250   # 1: Manual Mode 3: Aperture Priority Mode default:3
-    #v4l2-ctl -d /dev/video2 -c exposure_auto=1   # min=78 max=1250 step=1 default=312
-    #v4l2-ctl -d /dev/video2 -c exposure_absolute=1250   # 1: Manual Mode 3: Aperture Priority Mode default:3
+    v4l2-ctl -d /dev/video0 -c exposure_auto=1   # min=78 max=1250 step=1 default=312
+    v4l2-ctl -d /dev/video0 -c exposure_absolute=1250   # 1: Manual Mode 3: Aperture Priority Mode default:3
+    v4l2-ctl -d /dev/video2 -c exposure_auto=1   # min=78 max=1250 step=1 default=312
+    v4l2-ctl -d /dev/video2 -c exposure_absolute=1250   # 1: Manual Mode 3: Aperture Priority Mode default:3
 else
     # day
 
     # mjpg-streamer1
-    v4l2-ctl -d /dev/video0 -c exposure_time_absolute=50   # min=1 max=10000 step=1 default=1000
-    v4l2-ctl -d /dev/video0 -c auto_exposure=1   # 0: Auto Mode 1: Manual Mode default=0
+    #v4l2-ctl -d /dev/video0 -c exposure_time_absolute=50   # min=1 max=10000 step=1 default=1000
+    #v4l2-ctl -d /dev/video0 -c auto_exposure=1   # 0: Auto Mode 1: Manual Mode default=0
 
     # mjpg-streamer2
-    #v4l2-ctl -d /dev/video0 -c exposure_absolute=312   # min=78 max=1250 step=1 default=312
-    #v4l2-ctl -d /dev/video0 -c exposure_auto=3   # 1: Manual Mode 3: Aperture Priority Mode default:3
-    #v4l2-ctl -d /dev/video2 -c exposure_absolute=312   # min=78 max=1250 step=1 default=312
-    #v4l2-ctl -d /dev/video2 -c exposure_auto=3   # 1: Manual Mode 3: Aperture Priority Mode default:3
+    v4l2-ctl -d /dev/video0 -c exposure_absolute=312   # min=78 max=1250 step=1 default=312
+    v4l2-ctl -d /dev/video0 -c exposure_auto=3   # 1: Manual Mode 3: Aperture Priority Mode default:3
+    v4l2-ctl -d /dev/video2 -c exposure_absolute=312   # min=78 max=1250 step=1 default=312
+    v4l2-ctl -d /dev/video2 -c exposure_auto=3   # 1: Manual Mode 3: Aperture Priority Mode default:3
 fi
